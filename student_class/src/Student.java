b9f0404fd87fc056ef92b5892dffb3fd046d9e02
codete <codete@Mareks-MBP.codete>
2020-01-27 17:01:08 +0100

    finished array list

--- student_class/src/Student.java
+++ student_class/src/Student.java
@@ -1,46 +1,44 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.stream.IntStream;
 
 public class Student {
 
 	private String name;
-	private int[] marks;
+	private ArrayList<Integer> marks = new ArrayList<Integer>();
 
 	public Student(String name, int... marks) {
 		this.name = name;
-		this.marks = marks;
+
+		for (int mark : marks) {
+			this.marks.add(mark);
+		}
 	}
 
 	public int getNumberOfMarks() {
-		return marks.length;
+		return marks.size();
 
 	}
 
 	public int totalSumOfMarks() {
-		return IntStream.of(marks).sum();
+		int sum = 0;
+		for (int mark : marks) {
+			sum += mark;
+		}
+		return sum;
 	}
 
 	public int getMaxMark() {
-		int maxMark = Integer.MIN_VALUE;
-		for (int mark : marks) {
-			if (mark > maxMark) {
-				maxMark = mark;
-			}
 
-		}
-		return maxMark;
+		return Collections.max(marks);
 	}
 
 	public int getMinMark() {
-		int minMark = Integer.MAX_VALUE;
-		for (int mark : marks) {
-			if (mark < minMark) {
-				minMark = mark;
-			}
 
-		}
-		return minMark;
+		return Collections.min(marks);
 	}
 
 	public BigDecimal getAverageMarks() {
@@ -48,6 +46,19 @@ public class Student {
 		int number = getNumberOfMarks();
 		BigDecimal averageMark = new BigDecimal(sum).divide(new BigDecimal(number), 3, RoundingMode.UP);
 		return averageMark;
+	}
+
+	public String toString() {
+		return name + marks;
+	}
+
+	public void addNewMark(int mark) {
+		marks.add(mark);
+
+	}
+
+	public void removeMarkAtIndex(int index) {
+		marks.remove(index);
 	}
 
 }
--- student_class/src/StudentRunner.java
+++ student_class/src/StudentRunner.java
@@ -3,9 +3,9 @@ import java.math.BigDecimal;
 public class StudentRunner {
 
 	public static void main(String[] args) {
-		Student student1 = new Student("Alina", 9,9,7, 10, 8);
-		Student student2 = new Student("Kate", 9,9,9, 5);
-		int number= student1.getNumberOfMarks();
+		Student student1 = new Student("Alina", 9, 9, 7, 10, 8);
+		Student student2 = new Student("Kate", 9, 9, 9, 5);
+		int number = student1.getNumberOfMarks();
 		System.out.println("Number of marks " + number);
 		int sum = student1.totalSumOfMarks();
 		System.out.println("Sum of marks " + sum);
@@ -15,6 +15,10 @@ public class StudentRunner {
 		System.out.println("Min mark " + minMark);
 		BigDecimal average = student1.getAverageMarks();
 		System.out.println("Average mark " + average);
+		student1.addNewMark(35);
+		System.out.println(student2);
+		student1.removeMarkAtIndex(1);
+		System.out.println(student1);
 	}
 
 }
